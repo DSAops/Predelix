0cbd10f30c15571ca6bdb3fb1dc7a1de28aca012
Officialanuj01 <officialanuj004@gmail.com>
2025-07-03 16:27:35 +0530

    issue resolve for page not loading

--- client-side/src/Router.jsx
+++ client-side/src/Router.jsx
@@ -1,5 +1,6 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
+import About from "./pages/About";
 
 export default function AppRouter() {
   return (
